7d33441ecf8803a258053c418f62bac61eed16fa
Lara <l.tchani37@gmail>
2022-04-07 14:19:27 -0400

    add sln

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,4 +1,8 @@
 using Xunit;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+using Models;
 
 namespace Test;
 
@@ -7,6 +11,15 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        //Arrange
+        //I arranged for this test by creating a new user to set the lastname as
+        User testUser = new User();
+
+        //Act
+        testUser.LastName = "Test LastName";
+
+        //Assert that testUser's title is the thing that we set
+        Assert.Equal("Test LastName", testUser.LastName);
 
     }
 }
